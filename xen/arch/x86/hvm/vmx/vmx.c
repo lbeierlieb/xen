9438d66bd7040ca6d23e7ee1329953f77f59c290
Lukas Beierlieb <lukas.beierlieb@cyberus-technology.de>
2026-01-29 15:49:17 +0100

    fix format string

--- xen/arch/x86/hvm/vmx/vmx.c
+++ xen/arch/x86/hvm/vmx/vmx.c
@@ -4164,7 +4164,7 @@ void asmlinkage vmx_vmexit_handler(struct cpu_user_regs *regs)
         p2m_set_altp2m(v, idx);
     }
 
-    printk(XENLOG_ERR "vmexit %d\n", exit_reason);
+    printk(XENLOG_ERR "vmexit %ld\n", exit_reason);
     if ( unlikely(currd->arch.monitor.vmexit_enabled) )
     {
         int rc;
